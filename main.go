e87a3577c9b701fef2555fa1368a27b3c43bdb11
Majid Mokhtari <majidmokht@gmail.com>
2018-02-10 22:36:05 -0800

    test

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/user/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, finch.\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleHl"))
 }
